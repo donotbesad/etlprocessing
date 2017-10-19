e82fb302e5534be92a1550d3a7bd607f8ff29001
Dmitry Artemenko <dmtry.artemenko@gmail.com>
2017-10-20 00:46:56 +0200

    init commit

---
+++ src/main/java/com/dart/EtlProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.dart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtlProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtlProcessingApplication.class, args);
+	}
+}
---
+++ src/main/java/com/dart/configuration/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.dart.configuration;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by dart on 20.10.17.
+ */
+
+@Configuration
+public class DatabaseConfig {
+
+   @Bean
+   @Primary
+   @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
