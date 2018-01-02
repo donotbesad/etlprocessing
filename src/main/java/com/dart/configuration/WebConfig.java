371e9a5884fe0a3dd5f3a1b73b8c4a8276ac308d
Dmitry Artemenko <dmtry.artemenko@gmail.com>
2018-01-02 16:35:19 +0100

    Front-end part set up (beginning)

---
+++ src/main/java/com/dart/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package com.dart.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Dmitry Artemenko
+ * 1/2/2018
+ */
+
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry
+//                .addResourceHandler("/**")
+//                .addResourceLocations("/**");
+//    }
+//}
