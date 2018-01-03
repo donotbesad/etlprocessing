c698b8df5c3dd0456b647dc82decdbbd6fd7fd14
Dmitry Artemenko <dmytro.artemenko@globallogic.com>
2018-01-03 12:57:42 +0100

    Front-end part setting up resource handler

--- src/main/java/com/dart/configuration/WebConfig.java
+++ src/main/java/com/dart/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.dart.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,14 +11,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  * 1/2/2018
  */
 
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig extends WebMvcConfigurerAdapter {
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry
-//                .addResourceHandler("/**")
-//                .addResourceLocations("/**");
-//    }
-//}
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/js/**")
+                .addResourceLocations("classpath:/static/js/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
